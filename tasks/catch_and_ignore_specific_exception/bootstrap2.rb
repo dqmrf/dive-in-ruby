05f61068bfb9c75fd18212a1c48998733cb25dce
misha-pelykh <mpel9797dev@gmail.com>
2017-11-26 10:14:02 +0200

    change bootstrap2.rb content

--- tasks/catch_and_ignore_specific_exception/bootstrap2.rb
+++ tasks/catch_and_ignore_specific_exception/bootstrap2.rb
@@ -3,34 +3,44 @@ require_relative 'my_error'
 require_relative 'stripe_error'
 
 class SubscriptionsHelper
+  attr_reader :error
+
   def charge
-    error_interceptor do
-      update_subscription
-    end
+    # raise StripeError.new('Error in :charge method!')
+    update_subscription
+    # raise StripeError.new('Error 2 in :charge method!')
+    puts '>> :charge CODE EXECUTED!'
+    true
+  rescue => e
+    handle_error(e)
   end
 
   private
 
   def update_subscription
-    raise StripeError.new("Couldn't update subscription plan!")
+    # raise StripeError.new("Couldn't update subscription plan!")
+    puts '>> :update_subscription CODE EXECUTED!'
   rescue => e
-    call_error(e)
+    pass_error(e)
   end
 
-  def error_interceptor
-    begin
-      # @stop = Proc.new {|e| raise e }
-      yield if block_given?
-    rescue => e
-      puts "Error: #{e.message}"
-    end
+  def pass_error(err)
+    log_error(err)
+    raise
   end
 
-  def call_error(e)
-    raise e
-    # @stop.call(e)
+  def handle_error(err)
+    log_error(err)
+  end
+
+  def log_error(err)
+    self.error = err if @error.nil?
+  end
+
+  def error=(error)
+    @error = error
   end
 end
 
 sh = SubscriptionsHelper.new
-sh.charge
+p sh.charge
