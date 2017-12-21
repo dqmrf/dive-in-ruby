b59e41939ae6e30d7c74fa43e165f12c9c46daae
misha-pelykh <mpel9797dev@gmail.com>
2017-12-21 20:39:21 +0200

    add version with instance_eval to foo class

--- tasks/override_parent_class_method.rb
+++ tasks/override_parent_class_method.rb
@@ -26,7 +26,32 @@ class Bar < Foo
   end
 end
 
+class InsEval < Foo
+  def roles
+    roles = super
+
+    roles.instance_eval do
+      def method_missing(name)
+        name.to_s.match(/[a-z]+(?=\?$)/) do |match|
+          return self.any? do |role|
+            role[:name].to_s.eql?(match[0])
+          end
+        end
+        super
+      end
+    end
+
+    roles
+  end
+end
+
 bar = Bar.new
-p bar.roles
+ie = InsEval.new
+
+# p bar.roles
+
 p bar.roles.admin? #=> true
 p bar.roles.odmen? #=> false
+
+p ie.roles.admin?  #=> true
+p ie.roles.odmen?  #=> false
